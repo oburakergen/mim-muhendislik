16e3f4ccff997baabd3ef01f5be0fbde90b9e796
burak <burak.ergen@outlook.com.tr>
2023-11-24 00:28:29 +0300

    added footer

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,8 +1,56 @@
 import 'bootstrap';
 import Swiper from 'swiper';
+import {CountUp} from "countup.js";
 
 $(document).ready(() => {
+  const countUp = new CountUp('about-proje', 120,
+    {
+      duration: 3,
+      separator: '.',
+      prefix: '+',
+    });
+  if (!countUp.error) {
+    countUp.start();
+  } else {
+    console.error(countUp.error);
+  }
+
+  const countUp2 = new CountUp('about-partner', 30,
+    {
+      duration: 3,
+      separator: '.',
+      prefix: '+',
+    });
+  if (!countUp2.error) {
+    countUp2.start();
+  } else {
+    console.error(countUp2.error);
+  }
+
+  const countUp3 = new CountUp('about-ton', 30000, {
+    duration: 3,
+    separator: '.',
+    prefix: '+',
+  });
+  if (!countUp3.error) {
+    countUp3.start();
+  } else {
+    console.error(countUp3.error);
+  }
+
   const swiper = new Swiper('.mySwiper', {
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      renderBullet (index, className) {
+        return `<span class="${  className  }">${  index + 1  }</span>`;
+      },
+    },
+  });
+
+  const swiper4 = new Swiper('.mySwiper4', {
+    slidesPerView: 4,
+    spaceBetween: 30,
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
