8463b15c3e8f3e63a857a6bb9404dda5c96824d9
burak <burak.ergen@outlook.com.tr>
2023-11-25 04:01:40 +0300

    updated project

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -59,6 +59,18 @@ $(document).ready(() => {
       },
     },
   });
+
+  const swiper3 = new Swiper('.mySwiper3', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      renderBullet (index, className) {
+        return `<span class="${  className  }">${  index + 1  }</span>`;
+      },
+    },
+  });
 });
 
 window.$ = $;
